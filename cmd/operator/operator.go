00075f6c0d016296abb9b8f75246a961ba2b04b5
Dan Mace <ironcladlou@gmail.com>
2021-10-12 11:22:01 -0400

    Upstream: add a pprof endpoint

--- cmd/operator/operator.go
+++ cmd/operator/operator.go
@@ -16,8 +16,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"net/http"
+	"net/http/pprof"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -37,6 +43,7 @@ func main() {
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	flagConfigPath := flag.String("config", "", "Load configuration from the specified file.")
 	flagLogLevel := flag.String("log-level", "info", "Define the logging level.")
+	flagPprofAddr := flag.String("pprof-addr", "", "Serve pprof metrics on addr")
 	flag.Parse()
 
 	// Initialize logging system.
@@ -48,6 +55,43 @@ func main() {
 		zap.S().With(zap.Error(err)).Fatal("failed to load configuration")
 	}
 
+	if addr := *flagPprofAddr; len(addr) > 0 {
+		go startPprofServer(addr)
+	}
+
 	// Run.
 	operator.New(config.ECO).Run()
+}
+
+func startPprofServer(addr string) {
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, syscall.SIGTERM, syscall.SIGINT)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	server := http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	go func() {
+		<-shutdownChan
+		timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		if err := server.Shutdown(timeout); err != nil {
+			zap.S().With(zap.Error(err)).Info("error shutting down pprof server")
+		} else {
+			zap.S().Info("pprof server shutdown", "addr", addr)
+		}
+	}()
+	zap.S().Info("starting pprof server", "addr", addr)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		zap.S().With(zap.Error(err)).Fatalf("error starting pprof server")
+	} else {
+		zap.S().Info("pprof server returned", "addr", addr)
+	}
 }
